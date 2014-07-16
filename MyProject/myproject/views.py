b385a6f649a9620b00213571528173d5054454f9
John Lockwood <johnwlockwood@gmail.com>
2014-07-16 16:30:17 -0700

    Update views.py

--- MyProject/myproject/views.py
+++ MyProject/myproject/views.py
@@ -3,4 +3,6 @@ from pyramid.view import view_config
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'MyProject'}
+    assets_env = request.webassets_env
+    css_urls = assets_env['css_theme'].urls()
+    return {'project': 'MyProject', 'css_urls': css_urls}
