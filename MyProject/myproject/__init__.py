d2107d0b2b85b6cf6f4125fce9530367615b8fec
John Lockwood <johnwlockwood@gmail.com>
2014-07-16 15:44:37 -0700

    Wire up assets environment

--- MyProject/myproject/__init__.py
+++ MyProject/myproject/__init__.py
@@ -41,8 +41,16 @@ def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
     config = Configurator(settings=settings)
-    config.include('pyramid_chameleon')
+    config.add_jinja2_extension('webassets.ext.jinja2.AssetsExtension')
+    assets_env = config.get_webassets_env()
+    jinja2_env = config.get_jinja2_environment()
+    if jinja2_env:
+        jinja2_env.assets_environment = assets_env
+    webassets_env = config.get_webassets_env()
+    config.add_webasset('css_theme',
+                        _bundle_app_css(debug=webassets_env.debug))
     config.add_static_view('static', 'static', cache_max_age=3600)
+    config.include('pyramid_chameleon')
     config.add_route('home', '/')
     config.scan()
     return config.make_wsgi_app()
