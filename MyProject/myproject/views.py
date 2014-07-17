7b8c3a4e3052e1131ebb9b6ad6631a9f4e45badb
John.Lockwood <john.lockwood@webfilings.com>
2014-07-16 20:03:52 -0700

    Make it point to MyProject

--- MyProject/myproject/views.py
+++ MyProject/myproject/views.py
@@ -5,4 +5,4 @@ from pyramid.view import view_config
 def my_view(request):
     assets_env = request.webassets_env
     css_urls = assets_env['css_theme'].urls()
-    return {'project': 'MyProject', 'css_urls': css_urls}
+    return {'project': 'hello MyProject', 'css_urls': css_urls}
